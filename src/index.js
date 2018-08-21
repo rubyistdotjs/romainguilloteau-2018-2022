80efaf320d812dc8ae3953b2a4ac0c3f535bf36a
Romain Guilloteau <romain.guilloteau.pro@gmail.com>
2018-08-21 13:48:21 +0200

    Add react-snapshot

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-snapshot';
 import firebase from 'firebase/app';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
@@ -14,5 +14,5 @@ const config = {
 };
 firebase.initializeApp(config);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
 registerServiceWorker();

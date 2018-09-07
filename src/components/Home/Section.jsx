1f9a4eb37e0e4063aa8bbd3b3fd18fe5b344e8ee
Romain Guilloteau <romain.guilloteau.pro@gmail.com>
2018-09-07 13:45:14 +0200

    Remove useless components

--- src/components/Home/Section.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Section({ bgColor, children }) {
-  return (
-    <section className={`bg-${bgColor} py-8`}>
-      {children}
-    </section>
-  )
-}

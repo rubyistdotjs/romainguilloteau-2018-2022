0e5645ee6ba6efa6256c68af4682257ba00052e7
Romain Guilloteau <romain.guilloteau.pro@gmail.com>
2018-09-07 14:24:01 +0200

    Remove firebase inside the app

--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
 import React from 'react';
 import { render } from 'react-snapshot';
-import firebase from 'firebase/app';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
-const config = {
-  apiKey: 'AIzaSyDOxfIAHCu1TaCNLk4270bPcFn4g3x9NcA',
-  authDomain: 'romainguilloteau-af1c1.firebaseapp.com',
-  databaseURL: 'https://romainguilloteau-af1c1.firebaseio.com',
-  projectId: 'romainguilloteau-af1c1',
-  storageBucket: 'romainguilloteau-af1c1.appspot.com',
-  messagingSenderId: '348648773457',
-};
-firebase.initializeApp(config);
 
 render(<App />, document.getElementById('root'));
 registerServiceWorker();

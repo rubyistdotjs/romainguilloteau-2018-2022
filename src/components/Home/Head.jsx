c24e0e4da9132c3f2835fb0eb4726a34be10db55
rubyistdotjs <rubyistdotjs@gmail.com>
2019-01-03 08:58:33 +0100

    Add crunchbase profil url in head jsonld

--- src/components/Home/Head.jsx
+++ src/components/Home/Head.jsx
@@ -82,6 +82,7 @@ function Head({ intl }) {
               "https://www.linkedin.com/in/romainguilloteau",
               "https://twitter.com/rubyistdotjs",
               "https://plus.google.com/110337874986223414450",
+              "https://www.crunchbase.com/person/romain-guilloteau",
               "https://github.com/rubyistdotjs",
               "https://gitlab.com/rubyistdotjs"
             ]

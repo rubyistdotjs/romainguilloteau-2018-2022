1e5491888e4767fff250156228a8becf67efba9b
rubyistdotjs <rubyistdotjs@gmail.com>
2019-01-06 17:38:29 +0100

    Netlify!

---
+++ functions/src/getBookShelves.js
@@ -0,0 +1,35 @@
+require('../services/dotenv');
+
+const { getShelves, getShelfBooks } = require('../services/goodreads');
+
+function orderShelves(shelves) {
+  const order = process.env.GOODREADS_SHELVES.split(',').map(s => s.trim());
+  return shelves
+    .filter(s => order.includes(s.name))
+    .sort((a, b) => order.indexOf(a.name) - order.indexOf(b.name));
+}
+
+function fillShelves(shelves) {
+  return Promise.all(
+    shelves.map(shelf =>
+      getShelfBooks({ shelf: shelf.name }).then(books => ({
+        name: shelf.name,
+        books_count: shelf.book_count,
+        books,
+      }))
+    )
+  );
+}
+
+exports.handler = async function(event, context, callback) {
+  if (event.httpMethod !== 'GET') return { statusCode: 404, body: '' };
+
+  const unorderedShelves = await getShelves();
+  const orderedShelves = orderShelves(unorderedShelves);
+  const shelves = await fillShelves(orderedShelves);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(shelves),
+  };
+};

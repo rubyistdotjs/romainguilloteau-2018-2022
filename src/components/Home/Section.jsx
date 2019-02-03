ac39eeefb6628df0d47ef775ab63883836fd30ac
rubyistdotjs <rubyistdotjs@gmail.com>
2019-02-03 13:56:21 +0100

    Emoji component

--- src/components/Home/Section.jsx
+++ src/components/Home/Section.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Emoji from '../Emoji';
+
 function Section({ emoji, title, children }) {
   return (
     <section className="container my-24 md:my-32">
       <h2 className="text-4xl mb-16">
-        <span role="img" aria-label={title}>
-          {emoji}
-        </span>
+        <Emoji name={title} symbol={emoji} />
       </h2>
       {children}
     </section>

16ad780e14cbb39eea346f0382d920657771c429
rubyistdotjs <rubyistdotjs@gmail.com>
2019-03-20 13:47:26 +0100

    Improve redirect

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ import React from 'react';
 import { hydrate, render } from 'react-dom';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
-import App from './App';
 import { availableLocales, detectBrowserLocale } from './utils/locales';
 import * as serviceWorker from './serviceWorker';
+
+import App from './App';
 
 const rootDOMElement = document.getElementById('root');
 const root = (
   <BrowserRouter>
     <Switch>
       <Route path={`/:locale(${availableLocales.join('|')})`} component={App} />
-      <Redirect to={detectBrowserLocale()} />
+      <Redirect from="/*" to={`/${detectBrowserLocale()}/*`} />
     </Switch>
   </BrowserRouter>
 );

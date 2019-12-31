f2dbf0e04c04d7891070cfa497c3d6be8ee978f5
rubyistdotjs <rubyistdotjs@gmail.com>
2019-12-31 11:57:55 +0100

    Change app name to medium font weight

--- src/screens/HomeScreen/Jobs/Applications/Application.jsx
+++ src/screens/HomeScreen/Jobs/Applications/Application.jsx
@@ -10,15 +10,12 @@ function Application({ name, description, challenges, tags, offset }) {
       className="w-full flex-shrink-0 flex flex-col transition transition-transform"
       style={{ transform: `translateX(-${offset * 100}%)` }}
     >
-      <h4 className="text-gray-900 text-lg lg:text-2xl font-bold leading-tight mb-3">
+      <h4 className="text-gray-900 text-lg lg:text-2xl font-medium leading-tight mb-3">
         {name}
       </h4>
       {description &&
         description.split('\n').map((p, i) => (
-          <p
-            key={`description-${i}`}
-            className="text-gray-700 font-medium py-2"
-          >
+          <p key={`description-${i}`} className="text-gray-700 py-2">
             {p}
           </p>
         ))}
